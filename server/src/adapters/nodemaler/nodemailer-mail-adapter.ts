360da294b08225d72bd50fb6e841c61e5e50b8c7
Igor Lima <igorlimagn@gmail.com>
2022-05-10 21:51:16 -0300

    Stage 3

---
+++ server/src/adapters/nodemaler/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "393d2e0b476de2",
+      pass: "22c15a48ac058b"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Igor Lima <igorgoncalvesmc2@gmail.com>',
+            subject,
+            html: body,
+        });
+    }
+}
